d90977abf2b88c09b7293361a5e2037ee42d4513
rutrut6969 <rutledgeisaac6969@gmail.com>
2020-08-10 13:42:30 -0500

    Setting up about page

--- frontend/src/App/components/about/About.js
+++ frontend/src/App/components/about/About.js
@@ -3,8 +3,17 @@ import './about.css';
 
 export default function About() {
   return (
-    <div>
-      <h1>About Page</h1>
+    <div className='about'>
+      <h2>Let Me Tell You a Bit About Me</h2>
+      <p>
+        My name is <span>Isaac Rutledge</span> I'm a solo developer trying to
+        just make a living I'm here because I need to supply for my family. I've
+        had a lot of business ideas, and every single one of them has failed. So
+        I'm here now. Just trying to bring some bread to the table. I'm here to
+        do everything I can to make your internet dreams come true. I'm working
+        on developing some more skills, and I'm going to be making mobile
+        applications and such soon.
+      </p>
     </div>
   );
 }
--- frontend/src/App/components/home/home-components/BottomContent.jsx
+++ frontend/src/App/components/home/home-components/BottomContent.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+// TODO: Fix this up to read from a database and display other customers sites!
+
 export default function BottomContent() {
   return <div></div>;
 }
